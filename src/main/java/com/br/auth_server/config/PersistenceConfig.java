ca253832b15db06a55bed0dd47de691c0c5c3480
Marcos Ribeiro de Sousa <marcos.rib.sousa@gmail.com>
2026-05-08 14:36:53 -0300

    feat: adiciona consetimentos(scops) no banco de dados

--- src/main/java/com/br/auth_server/config/PersistenceConfig.java
+++ src/main/java/com/br/auth_server/config/PersistenceConfig.java
@@ -3,6 +3,7 @@ package com.br.auth_server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 
@@ -15,5 +16,14 @@ public class PersistenceConfig {
         RegisteredClientRepository registeredClientRepository
     ) {
         return new JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository);
+    }
+
+
+    @Bean
+    public JdbcOAuth2AuthorizationConsentService jdbcOAuth2AuthorizationConsentService(
+        JdbcTemplate jdbcTemplate,
+        RegisteredClientRepository registeredClientRepository
+    ) {
+        return new JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository);
     }
 }
